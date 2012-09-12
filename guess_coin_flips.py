4df8a1bb20e143549bc4b17d51b50336535e487d
katya soltan <illusivelf@gmail.com>
2012-09-11 23:05:05 -0400

    cleanup white space and quotations

--- guess_coin_flips.py
+++ guess_coin_flips.py
@@ -1,7 +1,7 @@
 # This is also a coin flip game but you must guess the number of heads.
 
 import random
-print('I will flip a coin 100 times. Guess how many times it/ll be heads!')
+print("I will flip a coin 100 times. Guess how many times it'll be heads!")
 guess_heads = input()
 heads = 0
 flips = 0
@@ -10,11 +10,10 @@ def your_guess(x, y):
         print('You are correct! Good job!')
     else:
         print('Sorry, there were actually ' + str(x) + ' heads out of 100 tosses.')
-    
+
 while flips < 100:
     if random.randint(0, 1) == 1:
         heads = heads + 1
     flips = flips + 1
     
 your_guess(heads, guess_heads)
-    
