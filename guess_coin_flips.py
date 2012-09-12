f1aae2aff0bdb6ea3600fc922f11bcfa2c2a5348
katya soltan <illusivelf@gmail.com>
2012-09-11 23:24:35 -0400

    change some numbers

--- guess_coin_flips.py
+++ guess_coin_flips.py
@@ -1,7 +1,7 @@
 # This is also a coin flip game but you must guess the number of heads.
 
 import random
-print("I will flip a coin 100 times. Guess how many times it'll be heads!")
+print("I will flip a coin 1000 times. Guess how many times it'll be heads!")
 guess_heads = input()
 heads = 0
 flips = 0
@@ -11,7 +11,7 @@ def your_guess(x, y):
     else:
         print('Sorry, there were actually ' + str(x) + ' heads out of 100 tosses.')
 
-while flips < 100:
+while flips < 1000:
     if random.randint(0, 1) == 1:
         heads += 1
     flips += 1
