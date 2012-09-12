ed1063d2c4118e7997ad034193c7988d1fa3f0ee
Evgeni Belin <ebelin@gmail.com>
2012-09-11 23:36:34 -0400

    Style changes
    Separated imports from code.  While there brought all variables close
    to where they are used.

--- guess_coin_flips.py
+++ guess_coin_flips.py
@@ -1,16 +1,18 @@
 # This is also a coin flip game but you must guess the number of heads.
 
 import random
+
 print("I will flip a coin 100 times. Guess how many times it'll be heads!")
 guess_heads = int(input())
-heads = 0
-flips = 0
+
 def your_guess(x, y):
     if x == y:
         print('You are correct! Good job!')
     else:
         print('Sorry, there were actually ' + str(x) + ' heads out of 100 tosses.')
 
+heads = 0
+flips = 0
 while flips < 100:
     if random.randint(0, 1) == 1:
         heads += 1
