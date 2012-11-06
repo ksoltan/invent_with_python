5aee097f0ef4a23a7f90437e0666d71aa552dc68
Katya Soltan <illusivelf@gmail.com>
2012-11-05 20:47:34 -0500

    Fixed random_word dereference

--- formatting.py
+++ formatting.py
@@ -4,19 +4,17 @@ words = 'eggs ham sausage pomegranite banana olives chicken'.split()
 
 def random_word(wordList):
     x = random.randint(0, len(wordList)-1)
-    random_word = words[x]
-    return random_word
+    return words[x]
 
 print('What is your name?')
-name = input()
+name = raw_input()
 play_again = False
 
-def say_what_you_like(name, random_word):
-    print(''''Hello {}, I know that you like to eat {}
-for breakfast, right?'''.format(name, random_word))
+def say_what_you_like(name, rword):
+    print(str('Hello {0}, I know that you like ' +
+              'to eat {1} for breakfast, right?').format(name, rword))
     
-random_word(words)
-say_what_you_like(name, random_word)
+say_what_you_like(name, random_word(words))
 
-if input() == True:
+if raw_input() == 'yes':
     play_again = True    
