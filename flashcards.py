faf4cf2d1902dd07704e76c045b68d01d399e235
Katya Soltan <illusivelf@gmail.com>
2012-11-27 19:59:30 -0500

    Minesweeper that Opens
    worked together. Showed recursion

---
+++ flashcards.py
@@ -0,0 +1,21 @@
+# Recursion: quizzes you on vocab
+import random
+import copy
+import sys
+
+WORDLIST = [['silicon', 'Si'], ['barium', 'Ba'], ['Tellurium', 'Te'], ['Tungsten', 'W'], ['Iodine', 'I']]
+random.shuffle(wordList)
+
+def randomWord(words):
+  if not words:
+    print 'Good job'
+    return
+  else:
+    print words[0][0]
+    if raw_input() == words[0][1]:
+      randomWord(words[1:])
+    else:
+      random.shuffle(words)
+      randomWord(words)
+      
+randomWord(WORDLIST)
