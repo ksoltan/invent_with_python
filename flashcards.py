8f327df4e848b6094cd0bf0c09532bb15ff8fcfe
Katya Soltan <illusivelf@gmail.com>
2012-12-10 22:01:42 -0500

    Para V Flashcards
    Studied Spanish!!

--- flashcards.py
+++ flashcards.py
@@ -1,22 +1,22 @@
 # Recursion: quizzes you on vocab
 
 import random
-import copy
-import sys
 
-WORDLIST = [['silicon', 'Si'], ['barium', 'Ba'], ['Tellurium', 'Te'], ['Tungsten', 'W'], ['Iodine', 'I']]
+WORDLIST = [['Adonde', 'to where'], ['Que', 'what'], ['Donde', 'where'], ['Quienes', 'whom'], ['Por que', 'why'],
+['Cuando', 'when'], ['Como', 'how'], ['Cuanto', 'how much'], ['Quien', 'who'], ['Cuantos/Cuantas', 'how many']]
 random.shuffle(WORDLIST)
 
 def randomWord(words):
   if not words:
-    print 'Good job'
+    print 'bye'
     return
   else:
     print words[0][0]
-    if raw_input() == words[0][1]:
+    if raw_input().lower() == words[0][1]:
+      print 'Good job...'
       randomWord(words[1:])
     else:
       random.shuffle(words)
       randomWord(words)
       
-# randomWord(WORDLIST)
+randomWord(WORDLIST)
