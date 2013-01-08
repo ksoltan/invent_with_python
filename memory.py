98b80bd393499286d20d3bcb66cb93e622d3c24e
Katya Soltan <illusivelf@gmail.com>
2013-01-07 19:41:59 -0500

    Memory with Classes

--- memory.py
+++ memory.py
@@ -1,121 +1,85 @@
 # This is a matching game
 import random
 import time
+import sys
 
-ROWS = 3
-COLUMNS = 2
-
-def getBoard(rows, columns):
-  num_range = range(columns * rows / 2)
-  all_nums = num_range * 2
-  random.shuffle(all_nums)
+class Memory:
+  def __init__(self, rows, cols):
+    # save number of rows and columns
+    self.rows = rows
+    self.cols = cols
+    # generate board: generate all numbers and fill in self.board
+    num_range = range(cols * rows / 2)
+    all_nums = num_range * 2
+    random.shuffle(all_nums)
+    self.board = []
+    for x in range(rows):
+      self.board.append([])
+      for y in range(cols):
+        self.board[x].append(all_nums.pop())
   
-  board = []
-  for x in range(rows):
-    board.append([])
-    for y in range(columns):
-      board[x].append(all_nums.pop())
-  return board
+  def showBoard(self, show_positions = [], show_all = False):
+    for row in range(len(self.board)):
+      row_values = self.board[row]
+      for col in range(len(row_values)):
+        value = "."
+        if show_all:
+          value = row_values[col]
+        elif [row, col] in show_positions:  
+          value = row_values[col]
+        print " ", value, " ",
+      print
+
+  def getSingleCard(self, msg):
+    print msg
+    print 'row = ',
+    row = int(raw_input())
+    print 'col = ',
+    col = int(raw_input())
+    return [row, col]
+
+  def getMove(self):
+    m1 = self.getSingleCard('First card:')
+    m2 = self.getSingleCard('Second card:')
+    return [m1, m2]
+
+  def getBoardValue(self, pos):
+    row, col = pos[0], pos[1]
+    return self.board[row][col]
+
+  def isGoodMove(self, positions):
+    val1 = self.getBoardValue(positions[0])
+    val2 = self.getBoardValue(positions[1])
+    return val1 == val2
   
-def printBoard(b):
-  print '    0123'
-  for r in b:
-    for c in range(r):
-      print "c", '.', "c",
-    print
-  print '    0123'
-    
-def isPositionInRange(positions, pos):
-  return pos in positions
+  def playAgain(self):
+    print 'Do you want to play again?'
+    return raw_input().lower().startswith('y')
 
-def showBoard(b, show_positions = [], show_all = False):
-  for row in range(len(b)):
-    row_values = b[row]
-    for col in range(len(row_values)):
-      value = "."
-      if show_all:
-        value = row_values[col]
-      elif [row, col] in show_positions:  
-        value = row_values[col]
-      print " ", value, " ",
-    print
+  def play(self):
+    numMoves = 0
+    goodMoves = []
+    while True:
+      print 'Your moves:', goodMoves
+      print 'Current board:'
+      self.showBoard(goodMoves)
+      move = self.getMove()
+      if self.isGoodMove(move):
+        goodMoves += move
+        if len(goodMoves) == self.rows * self.cols:
+          print 'You have solved the memory game.'
+          print 'Final board:'
+          self.showBoard(show_all = True)
+          if self.playAgain():
+            Memory(2, 3).play()
+          else:
+            sys.exit(0)
+      else:
+        self.showBoard(goodMoves + move)
+        time.sleep(3)
+        numMoves += 1
 
-def getSingleCard(msg):
-  print msg
-  print 'row = ',
-  row = int(raw_input())
-  print 'col = ',
-  col = int(raw_input())
-  return [row, col]
-
-def getMove():
-  m1 = getSingleCard('First card:')
-  m2 = getSingleCard('Second card:')
-  return [m1, m2]
-
-def getBoardValue(board, pos):
-  row, col = pos[0], pos[1]
-  return board[row][col]
-
-def isGoodMove(board, positions):
-  val1 = getBoardValue(board, positions[0])
-  val2 = getBoardValue(board, positions[1])
-  return val1 == val2
-
-def memoryGame():
-  b = getBoard(ROWS, COLUMNS)
-  numMoves = 0
-  goodMoves = []
+if __name__ == '__main__':
+  print 'Welcome to memory game. Enter the coordinates!'
   while True:
-    print 'Your moves:', goodMoves
-    print 'Current board:'
-    showBoard(b, goodMoves)
-    move = getMove()
-    if isGoodMove(b, move):
-      goodMoves += move
-      if len(goodMoves) == ROWS * COLUMNS:
-        print 'You have solved memory game.'
-        print 'Final board:'
-        printBoard(b)
-        break
-    else:
-      showBoard(b, goodMoves + move)
-      time.sleep(5)
-      numMoves += 1
-print 'Welcome to memory game. Enter the coordinates!'
-while True:
-  memoryGame()
-
-
-
-
-##def getMatchingPairs(rows, columns): 
-##    number_pairs = rows*columns/2
-##    board_of_matches = []
-##    for i in range(number_pairs):
-##        board_of_matches.append(i) 
-##def getMatchingPairs(columns): 
-##    number_pairs = 18
-##    board_of_matches = []
-##    for x in range(columns):
-##        board_of_matches.append([])
-##        for i in range(18):
-##            board_of_matches.append(i)
-##            board_of_matches.append(i)
-##            random.shuffle(board_of_matches)
-##    return board_of_matches
-##def getMatchingPairs(columns):
-##    board_of_matches = []
-##    number_of_pairs = '012345678'*2
-##    number_of_pairs.split()
-##    for i in number_of_pairs:
-##        board_of_matches.append(random.sample(number_of_pairs, 1))
-##    return board_of_matches
-def getMatchingPairs():
-    pairs = '012345678'*2
-    pairs.split()
-    return pairs
-
-
-
-
+    Memory(2, 3).play()
