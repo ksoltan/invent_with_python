c01bda08ac8ed96a511d9f4beac68ef90f2b9ca7
Katya Soltan <illusivelf@gmail.com>
2013-01-27 17:45:15 -0500

    Revised Competition Code

--- competition.py
+++ competition.py
@@ -1,21 +1,31 @@
 # Given a string of numbers formed as follows: 1234567891011121314...  WHat is the 100th numeral?
 
-def fibonacci():
-  sequence = range(10000)
-  y = ''
-  for x in sequence[1:]:
-    y += str(x)
-  return y
+def SequenceOfNumbers(count):
+  long_str = ''
+  for x in range(1, count):
+    long_str += str(x)
+  return long_str
 
 # What is the 283d 7 in the sequence above?
-def compute():
-  z = main()
-  numbers = []
-  for x in range(10000):
-    if z[x] == str(7):
-      numbers.append([x, '7'])
-  print numbers
-  return numbers
+def FindNthDigit(sequence, digit_to_find, digit_count):
+  current_count = 0
+  current_position = 0
+  for digit in sequence:
+    current_position += 1
+    if digit == digit_to_find:
+      current_count += 1
+      if current_count == digit_count:
+        return current_position
+  return -1
+
+# print "Generating digit sequence"
+# x = SequenceOfNumbers(100)
+# print "sequence:", x
+# print "10th 7 position is", FindNthDigit(x, '7', 10)
+
+print "Generating digit sequence"
+x = SequenceOfNumbers(1000)
+print "283rd 7 position is", FindNthDigit(x, '7', 283)
 
 # What is the 15th term of a new Fibonacci Sequence starting from 4 and 7?
 #fibonacci = [4, 7]
@@ -31,13 +41,14 @@ def compute():
 #    Sequence(fibonacci[:y])
 
 def isDivisible(x):
-  numbers = range(x)
-  lists = numbers[2:]
-  for y in lists:
-    num = int(y)
-    ans = x/num
-    check = ans * num
-    if check == x:
-      print 'divisible by:', num
-      
+  for y in range(2, x/2 + 1):
+    if x%y == 0:
+      print 'divisible by:', y
+
+def isPrime(x):
+  for y in range(2, x/2 + 1):
+    if x%y == 0:
+      return False
+  return True
+
 
