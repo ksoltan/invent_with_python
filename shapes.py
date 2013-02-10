0a1a0e1330234f429faf7c570356115657cae5b9
Katya Soltan <illusivelf@gmail.com>
2013-02-09 21:58:47 -0500

    Classes with Shapes

--- shapes.py
+++ shapes.py
@@ -1,4 +1,5 @@
 import copy
+import math
 
 # Practicing classes:
 # Variables are calles Attributes
@@ -39,9 +40,25 @@ class Size:
   def scale(self, scale):
     return Size(self.w * scale, self.h * scale)
     
-class Rectangle:
-  def __init__(self, origin, size):
+
+class Shape2D:
+  """docstring for Shape"""
+  def __init__(self, origin):
     self.origin = origin
+
+  def area(self):
+    raise Exception('Must override in child class!')
+
+  def perimeter(self):
+    raise Exception('Must override in child class!')
+
+  def moveTo(self, new_origin):
+    """docstring for fname"""
+    self.origin = new_origin
+
+class Rectangle(Shape2D):
+  def __init__(self, origin, size):
+    Shape2D.__init__(self, origin)
     self.size = size
 
   def __str__(self):
@@ -58,3 +75,23 @@ class Rectangle:
     
   def translate(self, direction):
     return Rectangle(self.origin.translate(direction), copy.copy(self.size))
+
+class Circle(Shape2D):
+  def __init__(self, origin, radius):
+    Shape2D.__init__(self, origin)
+    self.radius = radius
+
+  def __str__(self):
+    return "Circle({0},{1})".format(str(self.origin), self.radius)
+
+  def area(self):
+    return math.pi * self.radius**2
+
+  def perimeter(self):
+    return 2 * math.pi * self.radius
+
+  def scale(self, scale):
+    return Circle(copy.copy(self.origin), self.radius * 2)
+
+  def translate(self, direction):
+    return Circle(self.origin.translate(direction), self.radius)
