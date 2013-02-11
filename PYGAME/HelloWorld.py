cbab5ad98523bc8c70dd70c9ba6c5d5e31450d7f
Katya Soltan <illusivelf@gmail.com>
2013-02-10 20:17:27 -0500

    PYGAME Hello World
    Just some color changes and experimenting

--- PYGAME/HelloWorld.py
+++ PYGAME/HelloWorld.py
@@ -11,37 +11,39 @@ pygame.display.set_caption('Hello World!')
 # Set up the colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+RED = (128, 0, 0)
+PURPLE = (128, 0, 128)
+TEAL = (0, 128, 128)
+YELLOW = (235, 235, 0)
 
 # Set up fonts
 basicFont = pygame.font.SysFont(None, 48)
 
 # Set up the text
-text = basicFont.render('Hello world!', True, WHITE, BLUE)
+text = basicFont.render('Hello world!', True, WHITE, TEAL)
 textRect = text.get_rect()
 textRect.centerx = windowSurface.get_rect().centerx
 textRect.centery = windowSurface.get_rect().centery
 
 # Draw the white background onto surface
 windowSurface.fill(WHITE)
-# Draw green polygon onto surface
-pygame.draw.polygon(windowSurface, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
+# Draw purple polygon onto surface
+pygame.draw.polygon(windowSurface, PURPLE, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
 
-# Draw some blue lines into surface
-pygame.draw.line(windowSurface, BLUE, (60, 60), (120, 60), 4)
-pygame.draw.line(windowSurface, BLUE, (120, 60), (120, 120))
-pygame.draw.line(windowSurface, BLUE, (60,121),(130, 120), 4)
+# Draw some teal lines into surface
+pygame.draw.line(windowSurface, TEAL, (60, 80), (140, 80), 5)
+pygame.draw.line(windowSurface, TEAL, (60, 80), (60, 140), 5)
+pygame.draw.line(windowSurface, TEAL, (140, 80), (140, 140), 5)
+pygame.draw.line(windowSurface, TEAL, (60,140),(140, 140), 5)
 
-# Draw blue circle onto surface
-pygame.draw.circle(windowSurface, BLUE, (300, 50), 20, 0)
+# Draw TEAL circle onto surface
+pygame.draw.circle(windowSurface, TEAL, (300, 50), 20, 0)
 
 # Draw red ellipse onto surface
 pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80), 1)
 
 # Draw text background onto surface
-pygame.draw.rect(windowSurface, RED, (textRect.left - 20, textRect.top - 20, textRect.width + 40, textRect.height + 40))
+pygame.draw.rect(windowSurface, YELLOW, (textRect.left - 20, textRect.top - 20, textRect.width + 40, textRect.height + 40))
 
 # Get pixel array of surface
 pixArray = pygame.PixelArray(windowSurface)
